3cc9a2fc80f3217511b282a337e6f5f44ebbc884
PCHer <yuan.pku@gmail.com>
2023-03-17 10:16:09 -0700

    Update index.php

--- packages/main/n2w/index.php
+++ packages/main/n2w/index.php
@@ -1,21 +1,22 @@
 <?php
-use NFNumberToWord\NumberToWords;
- 
+
 function main(array $args) : array
 {
-    if (!isset($args['number'])) {
-        return wrap(['error' => 'Please supply a number.']);
-    }
-    
-    $number = (int)($args['number']);
-    $words = (new NumberToWords)->toWords($number);
- 
-    return [
-        'body' => $words,
-    ];
-}
+    $favlink = "https://faas-sfo3-7872a1dd.doserverless.co/api/v1/web/fn-bf6699fb-503e-425d-b5de-773c664c1f5f/default/fav";
+    $brands = "https://faas-sfo3-7872a1dd.doserverless.co/api/v1/web/fn-bf6699fb-503e-425d-b5de-773c664c1f5f/default/brands";
+    $rss = "https://faas-sfo3-7872a1dd.doserverless.co/api/v1/web/fn-bf6699fb-503e-425d-b5de-773c664c1f5f/default/rss";
+    $greeting = "<h1><a href='$favlink'>Favorites</a> &nbsp; &nbsp; <a href='$brands'> Brands</a> &nbsp; &nbsp; <a href='$rss'> RSS</a></h1>";
 
-function wrap(array $args) : array
-{
-    return ["body" => $args];
+
+    echo $greeting;
+
+    $today = strtotime("today");
+    
+    for ($i = 0; $i < 20; $i++) {
+	    $datestr = date("Y/m/d", strtotime("-$i days", $today));
+	    $page .= "<h1><a href='day?date=$datestr' >$datestr</a></h1>";
+    }
+
+
+    return ["body" => $greeting.$page];
 }
